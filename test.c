b2d11023d5dbe566197ec325282c006abf075252
JLethe525 <2192969376@qq.com>
2026-07-23 22:05:29 +0800

    7月23号学习

--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+
 //int main()
 //{
 //	printf("%c",81);
 //	return 0;
 //}
-
 
 
 //int main() 
@@ -118,7 +119,7 @@
 //	return 0;
 //}
 
-    //int num;
+	//int num;
 	//printf("请输入数字：");
 	//scanf("%d", &num);
 	//if (num % 2 == 0)
@@ -126,7 +127,7 @@
 	//else
 	//	printf("jishu");
 
-    //int age = 0;
+	//int age = 0;
 	//scanf("%d", &age);
 	//if (age < 18)
 	//	printf("未成年\n");
@@ -172,6 +173,7 @@
 //	//printf((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? "是闰年\n" : "不是闰年\n");
 //}
 
+
 //int main()
 //{
 //	int i = 0, a = 1, b = 2, c = 3, d = 4;
@@ -180,6 +182,7 @@
 //	printf("%d", i);
 //	return 0;
 //}
+
 
 //int main()
 //{
@@ -214,6 +217,7 @@
 //	}
 //}
 
+
 //int main()
 //{
 //	int num =1 ;
@@ -237,6 +241,7 @@
 //	}
 //}
 
+
 //int main()
 //{
 //	for (int num = 1; num <= 10; num++)
@@ -254,6 +259,7 @@
 //	printf("%d", sum);
 //	return 0;
 //}
+
 
 //int i = 1;
 	//do
@@ -278,17 +284,193 @@
 //	return 0;
 //}
 
-//break和continue的用法
-int main()
-{
-	int i = 0;
-	while (i < 10)
-	{
-		i++;
-		if (i == 5)
-			continue;
-		printf("%d ", i);
-		
-	}
-	return 0;
-}
+
+//break和continue的用法：
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		i++;
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		
+//	}
+//	return 0;
+//}
+
+
+//循环嵌套
+//素数判断
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int j = 0;
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i % j == 0)
+//				break;
+//		}
+//
+//		if (i == j)
+//			printf("%d\n", i);
+//	}
+//	
+//	return 0;
+//}
+
+
+//优化方法
+//#include <math.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int j = 0;
+//		int judge = 1;//假设i是素数
+//		for (j = 2; j <=sqrt(i); j++)//写成i=a*b,有一个因子一定小于sqrt（i）
+//		{
+//			if (i % j == 0)
+//			{
+//				judge = 0;
+//				break;
+//			}
+//		}
+//		if (judge)
+//			printf("%d\n", i);
+//	}
+//	return 0;
+//}
+
+
+
+//使用goto语句的场景：在多个循环嵌套想要跳出来时
+//for (;)
+//{
+//	for (;)
+//	{
+//		for (;)
+//		{
+//			if (disaster)
+//				goto error;
+//		}
+//	}
+//}
+//
+//error:.....
+
+
+//关机程序
+//#include <stdlib.h>
+//#include <string.h>
+//int main()
+//{
+//	char input[50] = { 0 };
+//	system("shutdown -s -t 60");
+//again:
+//	printf("请注意，你的电脑在1分钟内关机\n");
+//	printf("输入我是小乐乐，取消关机\n");
+//	scanf("%s", input);
+//
+//	if (strcmp(input, "我是小乐乐")==0)
+//	{
+//		system("shutdown -a");
+//		printf("你很乖");
+//	}
+//	else
+//	    goto again;
+//
+//	return 0;
+//}
+
+
+//随机数生成
+// 
+//一.生成随机数的函数rand：  int rand(void);
+//
+//#include <stdlib.h>
+//int main()
+//{
+//	printf("%d\n", rand());
+//	printf("%d\n", rand());
+//	printf("%d\n", rand());
+//	printf("%d\n", rand());
+//	printf("%d\n", rand());
+//	return 0;
+//}
+// 
+//   其实rand函数⽣成的随机数是伪随机的，
+//   伪随机数不是真正的随机数，是通过某种算法⽣成的随机数
+//   ⽽rand函数是对⼀个叫“种⼦”的基准值进⾏运算⽣成的随机数
+//   之所以前⾯每次运⾏程序产⽣的随机数序列是⼀样的，
+//   那是因为rand函数⽣成随机数的默认种⼦是1。
+//   如果要⽣成不同的随机数，就要让种⼦是变化的
+//
+//二.函数srand⽤来初始化随机数的⽣成器：void srand (unsigned int seed);
+// 
+//   程序中在调⽤rand函数之前先调⽤srand函数，通过srand函数的参数seed来设置rand函数⽣成随
+//   机数的时候的种⼦，只要种⼦在变化，每次⽣成的随机数序列也就变化起来了。
+//   那也就是说给srand的种⼦是如果是随机的，rand就能⽣成随机数；
+//   在⽣成随机数的时候⼜需要⼀个随机数，这就⽭盾了。
+//
+//三.在程序中我们⼀般是使⽤程序运⾏的时间作为种⼦的，因为时间时刻在发⽣变化的。
+//   time函数:  time_t time (time_t* timer);
+//   time函数会返回当前的⽇历时间，其实返回的是1970年1⽉1⽇0时0分0秒到现在程序运⾏时间之间的
+//   差值，单位是秒。返回的类型是time_t类型的，
+//   time_t类型本质上其实就是32位或者64位的整型类型。
+//   time函数的参数timer如果是⾮NULL的指针的话，
+//   函数也会将这个返回的差值放在timer指向的内存中带回去。
+//   如果timer是NULL，就只返回这个时间的差值。
+//   time函数返回的这个时间差也被叫做：时间戳。
+//   time函数的时候需要包含头⽂件：time.h
+//   只是让time函数返回时间戳
+
+
+
+
+
+//
+//#include <math.h>
+//#include <stdlib.h>
+//#include <string.h>
+//void game()
+//{
+//	rand
+//
+//}
+//
+//
+//
+//int main()
+//{
+//	int input = 0;
+//	do
+//	{
+//		printf("--------------------------");
+//		printf("------   1.paly   --------");
+//		printf("------   2.exit   --------");
+//		printf("--------------------------");
+//		printf("请输入：");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();//函数
+//			printf("猜数字\n");
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("选择错误，重新选择");
+//			break;
+//		}
+//	} while (input);
+//
+//	return 0;
+//
+//}
